c0f3f1f69c01fc10cf446fff1fa1489d0525cda3
samocvetov <samocvetov@gmail.com>
2026-02-11 22:44:38 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,7 +1,12 @@
 $W="C:\ODT";if(!(Test-Path $W)){New-Item $W -ItemType Directory|Out-Null}
 $Log="$W\install.log";Start-Transcript -Path $Log -Append|Out-Null
-if(!(Test-Path "C:\Program Files\Microsoft Office\Root\Office16\WINWORD.EXE")){
-$E="$W\setup.exe";Invoke-WebRequest "https://s.id/office-x64" -OutFile $E -UseBasicParsing
+Stop-Process -Name setup -Force -ErrorAction SilentlyContinue
+Stop-Process -Name OfficeClickToRun -Force -ErrorAction SilentlyContinue
+if((winget source list) -match "msstore"){winget source remove msstore}
+$OfficeExe="C:\Program Files\Microsoft Office\Root\Office16\WINWORD.EXE"
+$E="$W\setup.exe"
+if(!(Test-Path $OfficeExe)){
+if(!(Test-Path $E)){Invoke-WebRequest "https://s.id/office-x64" -OutFile $E -UseBasicParsing}
 @"
 <Configuration>
 <Add OfficeClientEdition="64" Channel="Current">
@@ -15,13 +20,13 @@ $E="$W\setup.exe";Invoke-WebRequest "https://s.id/office-x64" -OutFile $E -UseBa
 $p=Start-Process $E -ArgumentList "/configure config.xml" -WorkingDirectory $W -Wait -PassThru
 if($p.ExitCode -eq 0){Write-Host "[ok] Microsoft 365"}else{Write-Host "[fail] Microsoft 365"}
 }else{Write-Host "[skip] Microsoft 365"}
-winget source update
-winget upgrade --all --silent --include-unknown --accept-package-agreements
+Start-Process winget -ArgumentList "source update" -NoNewWindow -Wait
+Start-Process winget -ArgumentList "upgrade --all --silent --include-unknown --accept-package-agreements --disable-interactivity --nowarn" -NoNewWindow -Wait
 $apps="7zip.7zip","Google.Chrome.EXE","Yandex.Browser","RustDesk.RustDesk","AnyDesk.AnyDesk","QL-Win.QuickLook","PDFgear.PDFgear","VideoLAN.VLC","AdrienAllard.FileConverter"
 foreach($i in $apps){
 $f=winget list --id $i -e 2>$null
 if($f -match $i){Write-Host "[skip] $i";continue}
-winget install --id $i -e --silent --disable-interactivity --accept-package-agreements
-if($LASTEXITCODE -eq 0){Write-Host "[ok] $i"}else{Write-Host "[fail] $i"}
+$p=Start-Process winget -ArgumentList "install --id $i -e --silent --accept-package-agreements --disable-interactivity --nowarn" -NoNewWindow -Wait -PassThru
+if($p.ExitCode -eq 0){Write-Host "[ok] $i"}else{Write-Host "[fail] $i"}
 }
 Stop-Transcript|Out-Null
