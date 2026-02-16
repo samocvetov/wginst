426a71a6c1cff37cc0b4a8dd396f094410a13ce9
samocvetov <samocvetov@gmail.com>
2026-02-17 00:19:23 +0300

    Update and rename oo.ps1 to o.ps1

---
+++ o.ps1
@@ -0,0 +1,30 @@
+Start-Transcript -Path "$env:TEMP\oobe_automation.log" -Force
+$TypeDef=@"
+using System;
+using System.Runtime.InteropServices;
+namespace Api{public class Kernel32{[DllImport("kernel32.dll",CharSet=CharSet.Auto,SetLastError=true)]public static extern int OOBEComplete(ref bool bIsOOBEComplete);}}
+"@
+try{Add-Type -TypeDefinition $TypeDef -Language CSharp -ErrorAction Stop}catch{Write-Error $_;Stop-Transcript;exit 1}
+$IsOOBEComplete=$false
+[Api.Kernel32]::OOBEComplete([ref]$IsOOBEComplete)|Out-Null
+if($IsOOBEComplete){Stop-Transcript;exit 0}
+Add-Type -AssemblyName System.Web
+$PasswordPlain=[System.Web.Security.Membership]::GeneratePassword(16,3)
+$Username="LocalAdmin"
+$AnswerFile="$env:TEMP\UnattendOOBE.xml"
+$UnattendXml=@"
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
+<settings pass="oobeSystem">
+<component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+<OOBE><HideEULAPage>true</HideEULAPage><HideOEMRegistrationScreen>true</HideOEMRegistrationScreen><HideOnlineAccountScreens>true</HideOnlineAccountScreens><HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE><ProtectYourPC>3</ProtectYourPC></OOBE>
+<UserAccounts><LocalAccounts>
+<LocalAccount wcm:action="add"><Name>$Username</Name><Group>Administrators</Group><Password><Value>$PasswordPlain</Value><PlainText>true</PlainText></Password></LocalAccount>
+</LocalAccounts></UserAccounts>
+</component></settings></unattend>
+"@
+try{$UnattendXml|Out-File -FilePath $AnswerFile -Encoding utf8 -Force}catch{Write-Error $_;Stop-Transcript;exit 1}
+$SysprepPath="$env:SystemRoot\System32\Sysprep\sysprep.exe"
+if(-not(Test-Path $SysprepPath)){Write-Error "Sysprep not found";Stop-Transcript;exit 1}
+Start-Process -FilePath $SysprepPath -ArgumentList "/reboot /oobe /unattend:$AnswerFile" -Wait
+Stop-Transcript
